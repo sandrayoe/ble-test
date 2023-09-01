a63f48f146787ce7d0c7f8895494c516c76fcd1f
sandrayoe <60214652+sandrayoe@users.noreply.github.com>
2023-09-01 12:04:28 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,10 @@ bluetooth.onBluetoothConnected(function () {
 bluetooth.onBluetoothDisconnected(function () {
     connected = 0
 })
+let time2 = 0
+let time1 = 0
+let count_signal = 0
+let threshold = 0
 let signal = 0
 let connected = 0
 connected = 0
@@ -21,6 +25,19 @@ basic.forever(function () {
         signal = pins.analogReadPin(AnalogPin.P0)
         bluetooth.uartWriteLine("" + convertToText(Math.round(signal)) + " " + convertToText(Math.round(signal)))
         serial.writeLine("" + (Math.round(signal)))
-        led.toggle(0, 0)
+        threshold = 500
+        if (Math.round(signal) == threshold && count_signal == 0) {
+            time1 = input.runningTime()
+            count_signal = 1
+            serial.writeLine("" + (Math.round(signal)))
+        } else if (Math.round(signal) == threshold && count_signal == 1) {
+            count_signal = 2
+            serial.writeLine("" + (Math.round(signal)))
+        } else if (Math.round(signal) == threshold && count_signal == 2) {
+            time2 = input.runningTime()
+            serial.writeLine("" + (Math.round(signal)))
+        } else {
+            serial.writeLine("" + (Math.round(signal)))
+        }
     }
 })
